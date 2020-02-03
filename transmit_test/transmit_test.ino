e63cd7fe9cfcd5e0c402378b33e1ce56921f72e2
Rory Hayes <rorosaurus@gmail.com>
2020-02-03 14:51:35 -0800

    attach button to tx test

--- transmit_test/transmit_test.ino
+++ transmit_test/transmit_test.ino
@@ -5,6 +5,10 @@
   
 */
 #define transmitPin 2
+#define buttonPin 0 // connected between pin 1 and 3.3V
+#define ledPin 1
+
+bool oldButtonState = LOW;
 
 #include <RCSwitch.h>
 
@@ -24,35 +28,54 @@ void setup() {
   // mySwitch.setPulseLength(320);
   
   // Optional set number of transmission repetitions.
-  // mySwitch.setRepeatTransmit(15);
-  
+//   mySwitch.setRepeatTransmit(15);
+
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
+  // read the state of the pushbutton value:
+  bool buttonState = digitalRead(buttonPin);
 
-  /* See Example: TypeA_WithDIPSwitches */
-  mySwitch.switchOn("11111", "00010");
-  delay(1000);
-  mySwitch.switchOff("11111", "00010");
-  delay(1000);
+  if (buttonState == LOW && oldButtonState == HIGH) {
+    mySwitch.send(5393, 24);
+    
+    digitalWrite(ledPin, HIGH); 
+    delay(100);
+    digitalWrite(ledPin, LOW);
+    delay(100);
+    digitalWrite(ledPin, HIGH); 
+    delay(100);
+    digitalWrite(ledPin, LOW);
+    delay(100);
+  }
 
-  /* Same switch as above, but using decimal code */
-  mySwitch.send(5393, 24);
-  delay(1000);  
-  mySwitch.send(5396, 24);
-  delay(1000);  
+  oldButtonState = buttonState;
 
-  /* Same switch as above, but using binary code */
-  mySwitch.send("000000000001010100010001");
-  delay(1000);  
-  mySwitch.send("000000000001010100010100");
-  delay(1000);
-
-  /* Same switch as above, but tri-state code */ 
-  mySwitch.sendTriState("00000FFF0F0F");
-  delay(1000);  
-  mySwitch.sendTriState("00000FFF0FF0");
-  delay(1000);
+//  /* See Example: TypeA_WithDIPSwitches */
+//  mySwitch.switchOn("11111", "00010");
+//  delay(1000);
+//  mySwitch.switchOff("11111", "00010");
+//  delay(1000);
+//
+//  /* Same switch as above, but using decimal code */
+//  mySwitch.send(5393, 24);
+//  delay(1000);  
+//  mySwitch.send(5396, 24);
+//  delay(1000);  
+//
+//  /* Same switch as above, but using binary code */
+//  mySwitch.send("000000000001010100010001");
+//  delay(1000);  
+//  mySwitch.send("000000000001010100010100");
+//  delay(1000);
+//
+//  /* Same switch as above, but tri-state code */ 
+//  mySwitch.sendTriState("00000FFF0F0F");
+//  delay(1000);  
+//  mySwitch.sendTriState("00000FFF0FF0");
+//  delay(1000);
 
 //  delay(20000);
 }
