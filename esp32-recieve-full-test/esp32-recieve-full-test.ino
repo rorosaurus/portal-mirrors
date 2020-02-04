daef6f5cc85a9a1404e8999adf37caaa2c601047
Rory Hayes <rorosaurus@gmail.com>
2020-02-04 01:08:59 -0800

    more animation tweaking

--- esp32-recieve-full-test/esp32-recieve-full-test.ino
+++ esp32-recieve-full-test/esp32-recieve-full-test.ino
@@ -11,20 +11,19 @@ FASTLED_USING_NAMESPACE
 #define NUM_LEDS    90
 CRGB leds[NUM_LEDS];
 
-#define BRIGHTNESS 192 // 25/255 = 1/10th brightness
+#define BRIGHTNESS 192 // out of 255 total
 #define FRAMES_PER_SECOND 120
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
 
+#define rxPin 2
 RCSwitch mySwitch = RCSwitch();
 
-#define KEEP_SHOWING_FOR_MILLIS 60000
-long lastHeartbeat = millis();
+#define KEEP_SHOWING_FOR_MILLIS 60000 // after receiving radio input, animate the portal for KEEP_SHOWING_FOR_MILLIS milliseconds
+long lastRadioInput = millis();
 
-uint8_t blueHue = 130;  // old 212
-uint8_t orangeHue = 20; // old 89
-//
-//uint8_t purpleHue = ;
-//uint8_t yellowHue = ;
+uint8_t blueHue = 130;
+uint8_t orangeHue = 20;
+uint8_t gHue = 0; // rotating "base color" used by many of the patterns
 
 // setup function. runs once, then loop() runs forever
 void setup() {
@@ -36,63 +35,89 @@ void setup() {
   // set master brightness control
   FastLED.setBrightness(BRIGHTNESS);
 
-  pinMode(2, INPUT_PULLUP);
-  mySwitch.enableReceive(2);  // Receiver on interrupt 0 => that is pin #2 EXCEPT on ESP32, put the actual PIN number here
+  pinMode(rxPin, INPUT_PULLUP);
+  mySwitch.enableReceive(rxPin);  // Receiver on interrupt 0 => that is arduino pin #2 (EXCEPT on ESP32, we put the actual PIN number here)
 }
 
 // main loop, which executes forever
 void loop() {
-  if (mySwitch.available()) {
-    // TODO: read in portal color value
-    Serial.print("Received: ");
-    Serial.println( mySwitch.getReceivedValue() );
-    lastHeartbeat = millis();  
-    mySwitch.resetAvailable();
-  }
-
-//  if (millis() < lastHeartbeat + KEEP_SHOWING_FOR_MILLIS){
-  if (millis() % 20000 > 10000){
-    animatePortal(orangeHue);
-  }
-  else {
-    animatePortal(blueHue);
+  demo();
+  
+//  // handle incoming radio rx
+//  if (mySwitch.available()) {
+//    Serial.print("Radio received a value: ");
+//    long receivedValue = mySwitch.getReceivedValue();
+//    Serial.println(receivedValue);
+//    
+//    lastRadioInput = millis();  
+//    mySwitch.resetAvailable();
+//  }
+//
+//  // animate the portal if we recently recieved radio input
+//  if (millis() < lastRadioInput + KEEP_SHOWING_FOR_MILLIS){
+//    animatePortal(orangeHue);
+//  }
+//  else { // otherwise, turn off the LEDs
 //    fill_solid( leds, NUM_LEDS, CRGB::Black);
-  }
-//  juggle();
+//  }
   
   // send the 'leds' array out to the actual LED strip
   FastLED.show();  
   // insert a delay to keep the framerate modest
   FastLED.delay(1000/FRAMES_PER_SECOND);
+
+  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+}
+
+void demo() {
+  static bool oldOrange = false;
+  static bool oldBlue = false;
+  
+  if (millis() % 20000 > 10000){
+    if(oldOrange == false){
+      oldOrange = true;
+      oldBlue = false;
+      fill_solid( leds, NUM_LEDS, CRGB::Black);
+    }
+    animatePortal(orangeHue);
+  }
+  else {
+    if(oldBlue == false){
+      oldBlue = true;
+      oldOrange = false;
+      fill_solid( leds, NUM_LEDS, CRGB::Black);
+    }
+    animatePortal(blueHue);
+  }
 }
 
 // ====================
 // animation functions
 // ====================
 uint8_t numberOfPulses = 16;
-int phaseMultiplier = (360/numberOfPulses)*256;
+int phaseMultiplier = (360/numberOfPulses)*256; // maybe make this less exact? slightly random for each pulse, so the pattern isn't perfectly repeating?
 
 void animatePortal(uint8_t hue) {
-  // 16 colored dots, weaving in and out of sync with each other
-  fadeToBlackBy( leds, NUM_LEDS, 8); // 8/256 = dim by 1/32 each frame
+
+  fadeToBlackBy( leds, NUM_LEDS, 18); // out of 256 = dim by x/256th each frame. too much dimming will reveal the reds in orange.
+  
+  portalJuggle(1, hue);
+  portalJuggle(4, hue);
+  portalJuggle(5, hue);
+}
+
+void portalJuggle(uint8_t bpm, uint8_t hue) {
   for( int i = 0; i < numberOfPulses; i++) { // create numberOfPulses travelling pulses
-    uint8_t pos = beatsin16( 7, 0, NUM_LEDS-1, 0, i*phaseMultiplier); // calculate the position to set the color
+    uint8_t pos = beatsin16( bpm /* BPM, 1-7 seems ok */, 0, NUM_LEDS-1, 0, i*phaseMultiplier); // calculate the position to set the color
 
     // calculate an offset for the hue, based on the height of each pixel
     uint8_t hueOffset = 0;
     if (hue == blueHue) hueOffset = map(abs(pos-(NUM_LEDS/2)), 0, NUM_LEDS/2, 30, 0);
     if (hue == orangeHue) hueOffset = map(abs(pos-(NUM_LEDS/2)), 0, NUM_LEDS/2, 0, 30);
 
-    uint8_t valueOffset = map(abs(pos-(NUM_LEDS/2)), 0, NUM_LEDS/2, 96, 0);
+    uint8_t valueOffset = map(abs(pos-(NUM_LEDS/2)), 0, NUM_LEDS/2, 96, 0); // slightly dim the upper pixels
+    
     // assign the calculated color to the correct pixel
     leds[pos] |= CHSV(hue+hueOffset, 255, 255-valueOffset);
-  }
-}
-
-void juggle() {
-  // eight colored dots, weaving in and out of sync with each other
-  fadeToBlackBy( leds, NUM_LEDS, 20);
-  for( int i = 0; i < 8; i++) {
-    leds[beatsin16( i+7, 0, NUM_LEDS-1 )] |= CHSV(25, 255, 255);
   }
 }
---
+++ esp32-recieve-full-test/extra_animations.ino
@@ -0,0 +1,191 @@
+void rainbow() 
+{
+  // FastLED's built-in rainbow generator
+  fill_rainbow( leds, NUM_LEDS, gHue, 7);
+}
+
+void rainbowWithGlitter() 
+{
+  // built-in FastLED rainbow, plus some random sparkly glitter
+  rainbow();
+  addGlitter(80);
+}
+
+void addGlitter( fract8 chanceOfGlitter) 
+{
+  if( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB::White;
+  }
+}
+
+void confetti() 
+{
+  // random colored speckles that blink in and fade smoothly
+  fadeToBlackBy( leds, NUM_LEDS, 10);
+  int pos = random16(NUM_LEDS);
+  leds[pos] += CHSV( gHue + random8(64), 200, 255);
+}
+
+void sinelon()
+{
+  // a colored dot sweeping back and forth, with fading trails
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  int pos = beatsin16( 13, 0, NUM_LEDS-1 );
+  leds[pos] += CHSV( gHue, 255, 192);
+}
+
+void bpm()
+{
+  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
+  uint8_t BeatsPerMinute = 62;
+  CRGBPalette16 palette = PartyColors_p;
+  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
+  for( int i = 0; i < NUM_LEDS; i++) { //9948
+    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
+  }
+}
+
+void juggle() {
+  // eight colored dots, weaving in and out of sync with each other
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  byte dothue = 0;
+  for( int i = 0; i < 8; i++) {
+    leds[beatsin16( i+7, 0, NUM_LEDS-1 )] |= CHSV(dothue, 200, 255);
+    dothue += 32;
+  }
+}
+
+// This function draws rainbows with an ever-changing,
+// widely-varying set of parameters.
+void pride() 
+{
+  static uint16_t sPseudotime = 0;
+  static uint16_t sLastMillis = 0;
+  static uint16_t sHue16 = 0;
+ 
+  uint8_t sat8 = beatsin88( 87, 220, 250);
+  uint8_t brightdepth = beatsin88( 341, 96, 224);
+  uint16_t brightnessthetainc16 = beatsin88( 203, (25 * 256), (40 * 256));
+  uint8_t msmultiplier = beatsin88(147, 23, 60);
+
+  uint16_t hue16 = sHue16;//gHue * 256;
+  uint16_t hueinc16 = beatsin88(113, 1, 3000);
+  
+  uint16_t ms = millis();
+  uint16_t deltams = ms - sLastMillis ;
+  sLastMillis  = ms;
+  sPseudotime += deltams * msmultiplier;
+  sHue16 += deltams * beatsin88( 400, 5,9);
+  uint16_t brightnesstheta16 = sPseudotime;
+  
+  for( uint16_t i = 0 ; i < NUM_LEDS; i++) {
+    hue16 += hueinc16;
+    uint8_t hue8 = hue16 / 256;
+
+    brightnesstheta16  += brightnessthetainc16;
+    uint16_t b16 = sin16( brightnesstheta16  ) + 32768;
+
+    uint16_t bri16 = (uint32_t)((uint32_t)b16 * (uint32_t)b16) / 65536;
+    uint8_t bri8 = (uint32_t)(((uint32_t)bri16) * brightdepth) / 65536;
+    bri8 += (255 - brightdepth);
+    
+    CRGB newcolor = CHSV( hue8, sat8, bri8);
+    
+    uint16_t pixelnumber = i;
+    pixelnumber = (NUM_LEDS-1) - pixelnumber;
+    
+    nblend( leds[pixelnumber], newcolor, 64);
+  }
+}
+
+CRGBPalette16 pacifica_palette_1 = 
+    { 0x000507, 0x000409, 0x00030B, 0x00030D, 0x000210, 0x000212, 0x000114, 0x000117, 
+      0x000019, 0x00001C, 0x000026, 0x000031, 0x00003B, 0x000046, 0x14554B, 0x28AA50 };
+CRGBPalette16 pacifica_palette_2 = 
+    { 0x000507, 0x000409, 0x00030B, 0x00030D, 0x000210, 0x000212, 0x000114, 0x000117, 
+      0x000019, 0x00001C, 0x000026, 0x000031, 0x00003B, 0x000046, 0x0C5F52, 0x19BE5F };
+CRGBPalette16 pacifica_palette_3 = 
+    { 0x000208, 0x00030E, 0x000514, 0x00061A, 0x000820, 0x000927, 0x000B2D, 0x000C33, 
+      0x000E39, 0x001040, 0x001450, 0x001860, 0x001C70, 0x002080, 0x1040BF, 0x2060FF };
+
+
+void pacifica_loop()
+{
+  // Increment the four "color index start" counters, one for each wave layer.
+  // Each is incremented at a different speed, and the speeds vary over time.
+  static uint16_t sCIStart1, sCIStart2, sCIStart3, sCIStart4;
+  static uint32_t sLastms = 0;
+  uint32_t ms = GET_MILLIS();
+  uint32_t deltams = ms - sLastms;
+  sLastms = ms;
+  uint16_t speedfactor1 = beatsin16(3, 179, 269);
+  uint16_t speedfactor2 = beatsin16(4, 179, 269);
+  uint32_t deltams1 = (deltams * speedfactor1) / 256;
+  uint32_t deltams2 = (deltams * speedfactor2) / 256;
+  uint32_t deltams21 = (deltams1 + deltams2) / 2;
+  sCIStart1 += (deltams1 * beatsin88(1011,10,13));
+  sCIStart2 -= (deltams21 * beatsin88(777,8,11));
+  sCIStart3 -= (deltams1 * beatsin88(501,5,7));
+  sCIStart4 -= (deltams2 * beatsin88(257,4,6));
+
+  // Clear out the LED array to a dim background blue-green
+  fill_solid( leds, NUM_LEDS, CRGB( 2, 6, 10));
+
+  // Render each of four layers, with different scales and speeds, that vary over time
+  pacifica_one_layer( pacifica_palette_1, sCIStart1, beatsin16( 3, 11 * 256, 14 * 256), beatsin8( 10, 70, 130), 0-beat16( 301) );
+  pacifica_one_layer( pacifica_palette_2, sCIStart2, beatsin16( 4,  6 * 256,  9 * 256), beatsin8( 17, 40,  80), beat16( 401) );
+  pacifica_one_layer( pacifica_palette_3, sCIStart3, 6 * 256, beatsin8( 9, 10,38), 0-beat16(503));
+  pacifica_one_layer( pacifica_palette_3, sCIStart4, 5 * 256, beatsin8( 8, 10,28), beat16(601));
+
+  // Add brighter 'whitecaps' where the waves lines up more
+  pacifica_add_whitecaps();
+
+  // Deepen the blues and greens a bit
+  pacifica_deepen_colors();
+}
+
+// Add one layer of waves into the led array
+void pacifica_one_layer( CRGBPalette16& p, uint16_t cistart, uint16_t wavescale, uint8_t bri, uint16_t ioff)
+{
+  uint16_t ci = cistart;
+  uint16_t waveangle = ioff;
+  uint16_t wavescale_half = (wavescale / 2) + 20;
+  for( uint16_t i = 0; i < NUM_LEDS; i++) {
+    waveangle += 250;
+    uint16_t s16 = sin16( waveangle ) + 32768;
+    uint16_t cs = scale16( s16 , wavescale_half ) + wavescale_half;
+    ci += cs;
+    uint16_t sindex16 = sin16( ci) + 32768;
+    uint8_t sindex8 = scale16( sindex16, 240);
+    CRGB c = ColorFromPalette( p, sindex8, bri, LINEARBLEND);
+    leds[i] += c;
+  }
+}
+
+// Add extra 'white' to areas where the four layers of light have lined up brightly
+void pacifica_add_whitecaps()
+{
+  uint8_t basethreshold = beatsin8( 9, 55, 65);
+  uint8_t wave = beat8( 7 );
+  
+  for( uint16_t i = 0; i < NUM_LEDS; i++) {
+    uint8_t threshold = scale8( sin8( wave), 20) + basethreshold;
+    wave += 7;
+    uint8_t l = leds[i].getAverageLight();
+    if( l > threshold) {
+      uint8_t overage = l - threshold;
+      uint8_t overage2 = qadd8( overage, overage);
+      leds[i] += CRGB( overage, overage2, qadd8( overage2, overage2));
+    }
+  }
+}
+
+// Deepen the blues and greens
+void pacifica_deepen_colors()
+{
+  for( uint16_t i = 0; i < NUM_LEDS; i++) {
+    leds[i].blue = scale8( leds[i].blue,  145); 
+    leds[i].green= scale8( leds[i].green, 200); 
+    leds[i] |= CRGB( 2, 5, 7);
+  }
+}
